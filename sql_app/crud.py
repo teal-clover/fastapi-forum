f918a33bb564dd1100f4ca5d865548d586dbb037
Ivan <113459000+teal-clover@users.noreply.github.com>
2024-09-03 11:38:59 +0300

    Initial commit

---
+++ sql_app/crud.py
@@ -0,0 +1,50 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(
+        email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_posts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Post).offset(skip).limit(limit).all()
+
+
+def create_user_post(db: Session, post: schemas.PostCreate, user_id: int):
+    db_post = models.Post(**post.model_dump(), owner_id=user_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+
+def update_user(db: Session, user_id: int, user: schemas.UserUpdate):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    db_user.email=user.email
+    db.commit()
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    db.delete(db_user)
+    db.commit()
+    return None
---
+++ sql_app/schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str | None = None
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(UserBase):
+    pass
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    posts: list[Post] = []
+
+    class Config:
+        orm_mode = True
